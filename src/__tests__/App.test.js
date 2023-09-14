95956b01bf1b5257e2426b598192da4873ef7aca
dvcorreia1984 <victor@onsleer.co.za>
2023-09-14 10:16:16 +0200

    Fix Linter

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from '../App';
-import { store } from '../redux/store';
+import store from '../redux/store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import App from './App';
+import store from './redux/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
