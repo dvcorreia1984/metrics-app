f09b05c352fd93cd80548b72e467f9ac95da2a43
dvcorreia1984 <victor@onsleer.co.za>
2023-09-15 10:09:48 +0200

    Updated Home.js and countrySlice.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SouthernAfricaImage from '../assets/country.svg';
-import WesternAfricaImage from '../assets/country2.svg';
-import NorthernAfricaImage from '../assets/country3.svg';
-import EasternAfricaImage from '../assets/country4.svg';
-import CentralAfricaImage from '../assets/country5.svg';
-import AfricaImage from '../assets/africa.svg';
+// Home.js
 
-const Home = () => (
-  <div>
-    <div className="flex bg-[#5487e3] justify-evenly">
-      <div className="w-1/4 text-blue-950 p-5">
-        <img src={AfricaImage} alt="africa" />
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import SouthernAfricaImage from '../assets/country.svg';
+import NorthernAfricaImage from '../assets/country2.svg';
+import AfricaImage from '../assets/africa.svg';
+import { fetchSubregionData } from '../redux/country/countrySlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const [southernAfricaData, northernAfricaData] = useSelector((state) => [
+    state.countries.southernAfricaData,
+    state.countries.northernAfricaData,
+  ]);
+
+  useEffect(() => {
+    dispatch(fetchSubregionData('Southern Africa'));
+    dispatch(fetchSubregionData('Northern Africa'));
+  }, [dispatch]);
+
+  // Function to calculate the total population of a subregion
+  const calculatePopulation = (data) => {
+    if (data) {
+      return data.population;
+    }
+    return 'Loading...';
+  };
+
+  return (
+    <div>
+      <div className="flex bg-[#5487e3] justify-evenly">
+        <div className="w-1/4 text-blue-950 p-5">
+          <img src={AfricaImage} alt="africa" />
+        </div>
+        <div className="flex flex-col justify-center items-start text-white">
+          <h1 className="font-bold text-4xl">AFRICA</h1>
+          <h2>5.6780 views</h2>
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-start text-white">
-        <h1 className="font-bold text-4xl">AFRICA</h1>
-        <h2>5.6780 views</h2>
+      <div className="bg-[#35548b] pl-5 text-white font-bold">STATS BY COUNTRY</div>
+      <div className="flex flex-wrap justify-center bg-[#3f66ab]">
+        {/* Container div with flex styles */}
+        <div className="flex justify-between w-full max-w-screen-xl mx-auto p-4">
+          <Link to="/southern-africa" className="w-full sm:w-1/2 m-2 relative">
+            <BsArrowRightCircle className="text-white text-2xl absolute top-0 right-0" />
+            <div className="w-full aspect-square text-white font-bold p-5 flex flex-col items-center">
+              <img
+                src={SouthernAfricaImage}
+                alt="Southern Africa"
+                className="w-2/3 h-2/3 mx-auto object-cover"
+              />
+              <div className="w-full flex flex-col">
+                <span className="flex justify-end">SOUTHERN</span>
+                <span className="flex justify-end">AFRICA</span>
+                <span className="flex justify-end">
+                  {calculatePopulation(southernAfricaData)}
+                </span>
+              </div>
+            </div>
+          </Link>
+          <Link to="/northern-africa" className="w-full sm:w-1/2 m-2 relative">
+            <BsArrowRightCircle className="text-white text-2xl absolute top-0 right-0" />
+            <div className="w-full aspect-square text-white font-bold p-5 flex flex-col items-center">
+              <img
+                src={NorthernAfricaImage}
+                alt="Northern Africa"
+                className="w-2/3 h-2/3 mx-auto object-cover"
+              />
+              <div className="w-full flex flex-col">
+                <span className="flex justify-end">NORTHERN</span>
+                <span className="flex justify-end">AFRICA</span>
+                <span className="flex justify-end">
+                  {calculatePopulation(northernAfricaData)}
+                </span>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
-    <div className="bg-[#35548b] pl-5 text-white font-bold">STATS BY COUNTRY</div>
-    <div className="flex flex-wrap text-center bg-[#3f66ab]">
-      <Link to="/southern-africa" className="w-full sm:w-1/2">
-        <button type="button" className="w-3/4 aspect-square text-white font-bold p-5">
-          <img
-            src={SouthernAfricaImage}
-            alt="Southern Africa"
-            className="w-full h-full mx-auto"
-          />
-          SOUTHERN AFRICA
-        </button>
-      </Link>
-      <Link to="/western-africa" className="w-full sm:w-1/2 bg-[#4063a5]">
-        <button type="button" className="w-3/4 aspect-square text-white font-bold p-5">
-          <img src={WesternAfricaImage} alt="Western Africa" className="w-full h-full mx-auto" />
-          WESTERN AFRICA
-        </button>
-      </Link>
-      <Link to="/northern-africa" className="w-full sm:w-1/2 bg-[#4063a5]">
-        <button type="button" className="w-3/4 aspect-square text-white font-bold p-5">
-          <img src={NorthernAfricaImage} alt="Northern Africa" className="w-full h-full mx-auto" />
-          NORTHERN AFRICA
-        </button>
-      </Link>
-      <Link to="/eastern-africa" className="w-full sm:w-1/2">
-        <button type="button" className="w-3/4 aspect-square text-white font-bold p-5">
-          <img src={EasternAfricaImage} alt="Eastern Africa" className="w-full h-full mx-auto" />
-          EASTERN AFRICA
-        </button>
-      </Link>
-      <Link to="/central-africa" className="w-full sm:w-1/2">
-        <button type="button" className="w-3/4 aspect-square text-white font-bold p-5">
-          <img src={CentralAfricaImage} alt="Central Africa" className="w-full h-full mx-auto" />
-          CENTRAL AFRICA
-        </button>
-      </Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
--- src/redux/country/countrySlice.js
+++ src/redux/country/countrySlice.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 const initialState = {
   countries: [],
+  southernAfricaData: null,
+  northernAfricaData: null,
   status: 'idle',
   error: null,
 };
@@ -12,6 +14,11 @@ const initialState = {
 export const fetchCountries = createAsyncThunk('countries/fetchCountries', async () => {
   const response = await axios.get('https://restcountries.com/v2/region/africa');
   return response.data;
+});
+
+export const fetchSubregionData = createAsyncThunk('countries/fetchSubregionData', async (subregion) => {
+  const response = await axios.get(`https://restcountries.com/v2/subregion/${subregion}`);
+  return response.data[0]; // Take the first country's data
 });
 
 const countrySlice = createSlice({
@@ -30,6 +37,13 @@ const countrySlice = createSlice({
       .addCase(fetchCountries.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
+      })
+      .addCase(fetchSubregionData.fulfilled, (state, action) => {
+        if (action.meta.arg === 'Southern Africa') {
+          state.southernAfricaData = action.payload;
+        } else if (action.meta.arg === 'Northern Africa') {
+          state.northernAfricaData = action.payload;
+        }
       });
   },
 });
