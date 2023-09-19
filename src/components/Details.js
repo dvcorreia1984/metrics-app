853d8fd4ebf59c09324b35491d295f58100eaf73
dvcorreia1984 <victor@onsleer.co.za>
2023-09-19 14:29:15 +0200

    Updated Image of Details.js file

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import SouthernAfricaImage from '../assets/country.svg';
+import NorthernAfricaImage from '../assets/country2.svg';
+import MiddleAfricaImage from '../assets/country3.svg';
+import EasternAfricaImage from '../assets/country4.svg';
+import WesternAfricaImage from '../assets/country5.svg';
 
 const Details = () => {
   const { subregion } = useParams();
+  let subregionImage;
+
+  switch (subregion) {
+    case 'Southern Africa':
+      subregionImage = SouthernAfricaImage;
+      break;
+    case 'Northern Africa':
+      subregionImage = NorthernAfricaImage;
+      break;
+    case 'Middle Africa':
+      subregionImage = MiddleAfricaImage;
+      break;
+    case 'Eastern Africa':
+      subregionImage = EasternAfricaImage;
+      break;
+    case 'Western Africa':
+      subregionImage = WesternAfricaImage;
+      break;
+    default:
+      break;
+  }
   const countries = useSelector((state) => state.countries.countries);
 
   const subregionCountries = countries.filter(
@@ -17,9 +43,15 @@ const Details = () => {
 
   return (
     <div>
-      <div className="flex h-36 bg-[#5487e3] justify-center gap-10">
-        <div className="w-1/4 text-blue-950 py-3">
-          <img src="#" alt="africa" />
+      <div className="flex h-40 bg-[#5487e3] justify-center gap-10">
+        <div className="flex w-1/4 text-blue-950 py-3 items-center">
+          <div>
+            <img
+              src={subregionImage}
+              alt={subregion}
+              className="h-32 w-32"
+            />
+          </div>
         </div>
         <div className="flex flex-col justify-center items-start text-white">
           <h1 className="font-bold">{`${subregion.toUpperCase()}`}</h1>
@@ -35,12 +67,16 @@ const Details = () => {
               className="flex text-2xl h-24 p-5 items-center text-white gap-5 even:bg-[#35548b]"
             >
               <div
-                className="flex-1 pl-5"
+                className="w-7/12 pl-5"
               >
                 {country.name}
               </div>
-              <div>
+              <div
+                className="w-4/12"
+              >
                 {country.population}
+                {' '}
+                citizens
               </div>
               <BsArrowRightCircle />
             </li>
